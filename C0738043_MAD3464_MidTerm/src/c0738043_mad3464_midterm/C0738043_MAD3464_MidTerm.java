4d1dd0abb3ae8ddd79f466c34f261b1a0e684610
MacStudent <macstudent@20X-iMacXX-M-427.local>
2018-06-07 14:38:22 -0400

    first commit

---
+++ C0738043_MAD3464_MidTerm/src/c0738043_mad3464_midterm/C0738043_MAD3464_MidTerm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0738043_mad3464_midterm;
+
+/**
+ *
+ * @author macstudent
+ */
+public class C0738043_MAD3464_MidTerm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
